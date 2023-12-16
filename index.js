afe135a4527c4dff16ef74f5edf05496538654cf
ArpitShukla3 <arpit900shukla@gmail.com>
2023-12-16 09:18:23 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import userRoute from "./Routes/userRoute.js";
+import chatRoute from "./Routes/chatRoute.js"
+import cors from "cors";
+import dataBaseConnect from "./config/Connection.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+const app = express();
+dataBaseConnect();
+const PORT = process.env.PORT || 4000;
+//App Setup
+app.listen(PORT, function () {
+    console.log("server is listening at port", PORT);
+})
+app.use(cors());
+app.use(express.json()) // to accept json data
+app.use("/api/user", userRoute);
+app.use("/api/chat", chatRoute);
+app.use(notFound)
+app.use(errorHandler)        
