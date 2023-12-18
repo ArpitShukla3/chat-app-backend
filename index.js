8fab921f93c1e8219c21477cdf3e94bff6da82a1
ArpitShukla3 <arpit900shukla@gmail.com>
2023-12-18 13:29:37 +0530

    1st commit on 18-12-23

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoute from "./Routes/userRoute.js";
-import chatRoute from "./Routes/chatRoute.js"
+import chatRoute from "./Routes/chatRoute.js";
+import chatMessages from "./Routes/chatMessages.js";
 import cors from "cors";
 import dataBaseConnect from "./config/Connection.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
@@ -16,5 +17,6 @@ app.use(cors());
 app.use(express.json()) // to accept json data
 app.use("/api/user", userRoute);
 app.use("/api/chat", chatRoute);
+app.use("/api/message", chatMessages);
 app.use(notFound)
 app.use(errorHandler)        
